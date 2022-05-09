677f53e59ac66c937aa418310075786bab9f2915
allpavel <allpavel@yandex.ru>
2022-05-09 20:46:59 +0300

    added quick sort

---
+++ quickSort.js
@@ -0,0 +1,15 @@
+const quickSort = (arr) => {
+    if (arr.length < 2) return arr;
+    let pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (pivot > arr[i]) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+};
